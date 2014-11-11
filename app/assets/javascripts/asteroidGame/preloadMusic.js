dafd082a9db5e5825103a0f7c9256c11a8268350
Norris Nakagaki <norrisnakagaki@gmail.com>
2014-11-11 14:16:58 -0500

    fix music and image assets again and again

--- app/assets/javascripts/asteroidGame/preloadMusic.js
+++
@@ -1,8 +0,0 @@
-Asteroids.Music.gameAudio1 = new Audio("assets/test.mp3");
-Asteroids.Music.gameAudio2 = new Audio("assets/the-island-madeon-remix.mp3");
-Asteroids.Music.menuAudio = new Audio("assets/menu.mp3");
-
-Asteroids.Music.menuAudio.addEventListener('ended', function() {
-  this.currentTime = 0;
-  this.play();
-}, false);
