ff09b8163e8dd3e0ab3a70778b8e91841c656660
Norris Nakagaki <norrisnakagaki@gmail.com>
2015-08-11 21:54:56 -0400

    fix

--- app/assets/javascripts/projectsSection.js
+++ app/assets/javascripts/projectsSection.js
@@ -9,20 +9,24 @@ Portfolio.ProjectsSection = {
           snake     : this._snakeCallback
         };
 
+    this._connectVideos();
+
     $(".js-project-block").on("click", function(event) {
-      var projectName = $(event.currentTarget).data("project-name");
+      if(!event.target.className.match("collaborators")) {
+        var projectName = $(event.currentTarget).data("project-name");
 
-      $("body").append(JST[projectName]({
-        video_url : _this.videoUrls[projectName]
-      }));
+        $("body").append(JST[projectName]({
+          video_url : _this.videoUrls[projectName]
+        }));
 
-      if (projectCallbacks[projectName]) {
-        $("div.info-modal div.iframe").on("click", function (event) {
-          projectCallbacks[projectName]();
-        });
+        if (projectCallbacks[projectName]) {
+          $("div.info-modal div.iframe").on("click", function (event) {
+            projectCallbacks[projectName]();
+          });
+        }
+
+        _this._modalSetup();
       }
-
-      _this._modalSetup();
     });
 
     this._collab();
@@ -32,6 +36,11 @@ Portfolio.ProjectsSection = {
     }
 
     Portfolio.utils.autoPlayVideo($("video"));
+
+    this._resizeVideoAndImage();
+    $(window).on("resize", function (event) {
+      _.debounce(_this._resizeVideoAndImage, 150)();
+    });
   },
 
   _appendIframe: function(src) {
@@ -53,42 +62,55 @@ Portfolio.ProjectsSection = {
   },
 
   _collab: function () {
+    var _this = this;
+
     $(".projects li button.collaborators").one("click", function (event) {
-      var projectName = event.target.parentElement.parentElement.className;
+      var projectName = event.target.parentElement.parentElement.dataset.projectName;
 
       $(".projects li." + projectName).addClass("flip");
       setTimeout(function () {
         $(".projects li." + projectName).removeClass("flip");
-        collab();
+        _this._collab();
       }, 1500);
     });
   },
 
-  _ifSafari: function() {
-    $(".js-project-LLC")[0].src       = "<%= video_url('LLC_demo.mp4') %>";
-    $(".js-project-asteroids")[0].src = "<%= video_url('asteroids_gameplay_editted2.mp4') %>";
-    $(".js-project-princess")[0].src  = "<%= video_url('princess_demo_test.mp4') %>";
-    $(".js-project-snake")[0].src     = "<%= video_url('snake_gameplay_cropped.mp4') %>";
-    $(".js-project-trello")[0].src    = "<%= video_url('trello_demo_cropped.mp4') %>";
-    $(".js-project-chess")[0].src     = "<%= video_url('chess_demo.mp4') %>";
+  _connectVideos: function() {
+    $(".js-project-LLC")[0].src       = Portfolio.ProjectsSection.videoUrls.LLC;
+    $(".js-project-asteroids")[0].src = Portfolio.ProjectsSection.videoUrls.asteroids;
+    $(".js-project-princess")[0].src  = Portfolio.ProjectsSection.videoUrls.princess;
+    $(".js-project-snake")[0].src     = Portfolio.ProjectsSection.videoUrls.snake;
+    $(".js-project-trello")[0].src    = Portfolio.ProjectsSection.videoUrls.trello;
+    $(".js-project-chess")[0].src     = Portfolio.ProjectsSection.videoUrls.chess;
   },
 
   _modalSetup: function () {
+    var $infoModal = $(".info-modal");
+
     Portfolio.utils.autoPlayVideo($(".info-modal video"));
 
     setTimeout(function () {
-      $(".info-modal").removeClass("init")
-      $(".info-modal").addClass("mid")
+      $infoModal.removeClass("init")
+      $infoModal.addClass("mid")
 
       setTimeout(function () {
-        $(".info-modal").removeClass("mid")
-        $(".info-modal").addClass("final")
+        $infoModal.removeClass("mid")
+        $infoModal.addClass("final")
       }, 500);
     }, 100);
 
     $(".shadow").removeClass("hidden");
     window.yCorrToReturnTo = $(window).scrollTop();
     $(".window").addClass("hide-overflow");
+  },
+
+  _resizeVideoAndImage: function() {
+    var $projectLi    = $(".projects ul li"),
+        $projectImage = $(".projects ul li .back img"),
+        adjustedWidth = $projectLi.width() * 750 / 1250;
+
+    $projectLi.css("height", 51 + adjustedWidth);
+    $projectImage.css("height", adjustedWidth);
   },
 
   _snakeCallback: function() {
