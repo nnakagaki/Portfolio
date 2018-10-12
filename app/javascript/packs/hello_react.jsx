266062daf03c456b9ffc48c6bf1d525455036a34
Norris Nakagaki <norrisnakagaki@gmail.com>
2018-10-12 01:28:31 -0400

    more react

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
 import FlareLoading from '../components/FlareLoading';
 import StartScreen from '../components/StartScreen';
 
-const Hello = props => (
-  <div className="is-hidden">Hello {props.name}!</div>
-)
+const duration = 300;
 
-Hello.defaultProps = {
-  name: 'David'
+const defaultStyle = {
+  transition : `opacity ${duration}ms ease-in-out`,
+  opacity    : 0,
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
+const transitionStyles = {
+  entering : { opacity : 0 },
+  entered  : { opacity : 1 },
+};
+
+class Application extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading : true
+    };
+
+    this.documentLoadHandle();
+  }
+
+  documentLoadHandle() {
+    $(window).load(() => {
+      window.setTimeout(() => {
+        this.setState({ loading : false });
+      }, 1000);
+    });
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <div>
+        <Transition
+          in={loading}
+          timeout={300}
+        >
+          {(state) => (
+            <div style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+              <FlareLoading />
+            </div>
+          )}
+        </Transition>
+        <Transition
+          in={!loading}
+          timeout={300}
+        >
+          {(state) => (
+            <div style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+              <StartScreen />
+            </div>
+          )}
+        </Transition>
+      </div>
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <div>
-      <FlareLoading />
-      <StartScreen />
-    </div>,
+    <Application />,
     document.body.appendChild(document.createElement('div')),
   )
 })
