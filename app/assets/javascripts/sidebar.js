c522ff5ef012dbb547114164396f33ba8550531e
Norris Nakagaki <norrisnakagaki@gmail.com>
2018-10-14 00:08:00 -0400

    fix sidebar stuff

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -2,43 +2,9 @@ Portfolio.sidebar = {
   initialize: function() {
     var _this = this;
 
-    $(".side-bar li").on("click", function (event) {
-      $('html, body').animate({
-        scrollTop : $($("main ."+event.target.className).last()).offset().top
-      }, 300);
-    });
-
     this._scrollAllFunc();
     $(window).on("scroll", function(event) {
       _this._scrollAllFunc();
-    });
-
-    $("div.side-bar ul.links li a").on("mouseenter", function (event) {
-      $(event.currentTarget).addClass("hover-animation");
-      var intLength   = 2000,
-          intLength2  = 500,
-          timeoutFunc = function () {
-            heartbeat = setTimeout(function () {
-              intLength  = intLength * 0.9;
-              intLength2 = intLength2 * 0.9;
-              $(event.currentTarget).addClass("add-contrast");
-
-              heartbeat2 = setTimeout(function () {
-                $(event.currentTarget).removeClass("add-contrast");
-              }, intLength2);
-
-              timeoutFunc();
-            }, intLength);
-          };
-
-      timeoutFunc();
-    });
-
-    $(".side-bar ul.links li a").on("mouseleave", function (event) {
-      $(event.currentTarget).removeClass("hover-animation");
-      $(event.currentTarget).removeClass("add-contrast");
-      clearInterval(heartbeat);
-      clearTimeout(heartbeat2);
     });
   },
 
--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -1,32 +1,76 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-export default function Sidebar({ imageUrl }) {
-  return (
-    <div className="side-bar">
-      <div className="top-curve"></div>
-      <div className="top-curve2"></div>
-      <h1>Norris Nakagaki</h1>
-      <div className="triangle"></div>
-      <div className="top-circle"></div>
-      <img src={imageUrl} />
-      <ul>
-        <li className="intro"><div className="right-arrow"></div>Intro</li>
-        <li className="projects"><div className="right-arrow"></div>Projects</li>
-        <li className="skills"><div className="right-arrow"></div>Skills</li>
-        <li className="about"><div className="right-arrow"></div>About</li>
-        <li className="contact"><div className="right-arrow"></div>Contact</li>
-      </ul>
+export default class Sidebar extends PureComponent {
+  constructor(props) {
+    super(props);
 
-      <ul className="links">
-        <li><a className="github" href='https://github.com/nnakagaki' target='_blank'></a></li>
-        <li><a className="linkedin" href='https://www.linkedin.com/pub/norris-nakagaki/26/6b2/85b' target='_blank'></a></li>
-        <li><a className="codeeval" href='http://www.codeeval.com/profile/nnakagaki/' target='_blank'></a></li>
-        <li><a className="project-euler" href='https://projecteuler.net/profile/norichan.png' target='_blank'></a></li>
-        <li><a className="facebook" href='https://www.facebook.com/itsfinetoday' target='_blank'></a></li>
-        <li><a className="tumblr" href='https://www.tumblr.com/blog/nnakagaki' target='_blank'></a></li>
-        <li><a className="deviantart" href='http://norisuke.deviantart.com/' target='_blank'></a></li>
-        <li><a className="youtube" href='https://www.youtube.com/channel/UCd8sPxNL3EC_QTshgLbfqSw' target='_blank'></a></li>
-      </ul>
-    </div>
-  );
+    this.onClick = this.onClick.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  onClick(event) {
+    $('html, body').animate({
+      scrollTop : $($("main ."+event.target.className).last()).offset().top
+    }, 300);
+  }
+
+  onMouseEnter(event) {
+    $(event.currentTarget).addClass("hover-animation");
+    var intLength   = 2000,
+        intLength2  = 500,
+        timeoutFunc = () => {
+          this.heartbeat = setTimeout(() => {
+            intLength  = intLength * 0.9;
+            intLength2 = intLength2 * 0.9;
+            $(event.currentTarget).addClass("add-contrast");
+
+            this.heartbeat2 = setTimeout(() => {
+              $(event.currentTarget).removeClass("add-contrast");
+            }, intLength2);
+
+            timeoutFunc();
+          }, intLength);
+        };
+
+    timeoutFunc();
+  }
+
+  onMouseLeave(event) {
+    $(event.currentTarget).removeClass("hover-animation");
+    $(event.currentTarget).removeClass("add-contrast");
+    clearInterval(this.heartbeat);
+    clearTimeout(this.heartbeat2);
+  }
+
+  render() {
+    return (
+      <div className="side-bar">
+        <div className="top-curve"></div>
+        <div className="top-curve2"></div>
+        <h1>Norris Nakagaki</h1>
+        <div className="triangle"></div>
+        <div className="top-circle"></div>
+        <img src={imageUrl} />
+        <ul>
+          <li className="intro" onClick={this.onClick}><div className="right-arrow"></div>Intro</li>
+          <li className="projects" onClick={this.onClick}><div className="right-arrow"></div>Projects</li>
+          <li className="skills" onClick={this.onClick}><div className="right-arrow"></div>Skills</li>
+          <li className="about" onClick={this.onClick}><div className="right-arrow"></div>About</li>
+          <li className="contact" onClick={this.onClick}><div className="right-arrow"></div>Contact</li>
+        </ul>
+
+        <ul className="links">
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="github" href='https://github.com/nnakagaki' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="linkedin" href='https://www.linkedin.com/pub/norris-nakagaki/26/6b2/85b' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="codeeval" href='http://www.codeeval.com/profile/nnakagaki/' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="project-euler" href='https://projecteuler.net/profile/norichan.png' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="facebook" href='https://www.facebook.com/itsfinetoday' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="tumblr" href='https://www.tumblr.com/blog/nnakagaki' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="deviantart" href='http://norisuke.deviantart.com/' target='_blank'></a></li>
+          <li><a onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} className="youtube" href='https://www.youtube.com/channel/UCd8sPxNL3EC_QTshgLbfqSw' target='_blank'></a></li>
+        </ul>
+      </div>
+    );
+  }
 }
